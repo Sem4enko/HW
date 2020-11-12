7993de13b17390dc1b0d7ca7e4834631f2022065
Max <sem4enko_max@ukr.net>
2020-11-12 23:15:58 +0200

    First

---
+++ src/first.java
@@ -0,0 +1,43 @@
+public class first {
+    public static void main(String[] args) {
+        int a = 3345, b = -5475;
+        byte c = 100, d = 56;
+        short e = 3568, f = -3568;
+        long h = 2_340_321, g = 8_435_755_543L;
+        float i = 46.32F, j = 2.98F;
+        double k = -378.03, l = 745.31;
+
+        System.out.println("\nAddition :");
+        System.out.println("int: " + (a + b));
+        System.out.println("byte: " + (c + d));
+        System.out.println("short: " + (e + f));
+        System.out.println("long: " + (h + g));
+        System.out.println("float: " + (i + j));
+        System.out.println("double: " + (k + l));
+
+
+        System.out.println("\nSubtraction :");
+        System.out.println("int: " + (a - b));
+        System.out.println("byte: " + (c - d));
+        System.out.println("short: " + (e - f));
+        System.out.println("long: " + (h - j));
+        System.out.println("double: " + (k - l));
+
+        System.out.println("\nDivision :");
+        System.out.println("int: " + (a / b));
+        System.out.println("byte: " + (c / d));
+        System.out.println("short: " + (e / f));
+        System.out.println("long: " + (h / g));
+        System.out.println("float: " + (i / j));
+        System.out.println("double: " + (k / l));
+
+        System.out.println("\nMultiplication :");
+        System.out.println("int: " + (a * b));
+        System.out.println("byte: " + (c * d));
+        System.out.println("short: " + (e * f));
+        System.out.println("long: " + (h * g));
+        System.out.println("float: " + (i * j));
+        System.out.println("double: " + (k * l));
+
+    }
+}
