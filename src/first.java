21f023397648dd21e5202db1b80d38978f5b173d
Sem4enko <74368102+Sem4enko@users.noreply.github.com>
2020-11-13 14:34:34 +0200

    Update first.java

--- src/first.java
+++ src/first.java
@@ -3,7 +3,7 @@ public class First {
         int a = 3345, b = -5475;
         byte c = 100, d = 56;
         short e = 3568, f = -3568;
-        long h = 2_340_321L, g = 8_435_755_543L;
+        long h = 2340321L, g = 8435755543L;
         float i = 46.32F, j = 2.98F;
         double k = -378.03, l = 745.31;
 
@@ -14,7 +14,6 @@ public class First {
         System.out.println("long: " + (h + g));
         System.out.println("float: " + (i + j));
         System.out.println("double: " + (k + l));
-
 
         System.out.println("\nSubtraction :");
         System.out.println("int: " + (a - b));
